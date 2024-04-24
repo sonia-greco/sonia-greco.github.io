072ade3c8106600e29425a5c25fa589c65920c29
sonia-greco <144718680+sonia-greco@users.noreply.github.com>
2024-04-24 18:21:59 +0000

    created console.log

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+console.log("Task 1");
+
+console.log("Task 2");
+
+console.log("Task 3");
+
+console.log("Task 4");
+let sum = 0;
+//loop through 1 to 100 and add them to sum
+//print sum
+
+console.log("Task 5");
+//loop through 1 to 10
+let iFactorial = i;
+//loop through 1 to i
+//multiply ifactorial by your new number
+//printifactorial
