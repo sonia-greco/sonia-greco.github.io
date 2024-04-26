5e64dc9724f083660d671b8afa683da086a45292
sonia-greco <144718680+sonia-greco@users.noreply.github.com>
2024-04-26 19:41:59 +0000

    working on task 5

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,41 @@
 console.log("Task 1");
-for (let i = 1; i <= 10; i++)
- {
+for (let i = 1; i <= 10; i++) {
     let product = i * i;
-    print(product);
+    console.log(product);
 }
 
 console.log("Task 2");
+for (let i = 5; i > 0; i--) {
+    console.log(i);
+}
+    console.log("Blastoff!");
 
 console.log("Task 3");
+//for (let i = 1; i <= 25; i++) {
+//    let even = i * 2;
+//    console.log(even);
+//}
+for(let i = 2; i <= 50; i+=2){
+    console.log(i);
+}
 
 console.log("Task 4");
 let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum = sum + i;
+}
+console.log(sum);
 //loop through 1 to 100 and add them to sum
 //print sum
 
 console.log("Task 5");
 //loop through 1 to 10
-let iFactorial = i;
+for (let i = 1; i <= 10; i++) {
+    let iFactorial = i;
+    iFactorial = i * i;
+    console.log(iFactorial);
+}
+
 //loop through 1 to i
 //multiply ifactorial by your new number
 //printifactorial
