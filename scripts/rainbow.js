6ca02b1bbf3059eddb38435fde472446e2e09124
sonia-greco <144718680+sonia-greco@users.noreply.github.com>
2024-05-15 17:51:33 +0000

    completed creating dynamic content

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -1,1 +1,11 @@
-let arrcolor = ["maroon", "red", "purple", "fuchsia", "navy", "blue", "aqua"];
+let colorlist = ["maroon", "red", "purple", "fuchsia", "navy", "blue", "aqua"];
+let rainbowEl = document.createElement("ul");
+
+for (let i = 0; i < colorlist.length; i++) {
+    let listEl = document.createElement("li");
+    listEl.innerText = colorlist[i];
+    listEl.style.color = colorlist[i];
+    rainbowEl.appendChild(listEl);
+}
+document.body.appendChild(rainbowEl);
+console.log(listEl);
